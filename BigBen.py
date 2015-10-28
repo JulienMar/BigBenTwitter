7a6a5f31c18fe7350b2e31de83fe25d301974276
Julien <julien.marbaix@hotmail.com>
2015-10-28 02:21:57 +0100

    Get all tweet data

--- BigBen.py
+++ BigBen.py
@@ -12,6 +12,22 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-tweets = api.user_timeline(screen_name="big_ben_clock", count="25")
+def get_tweets():
+    tweets = api.user_timeline(screen_name="big_ben_clock", count="25")
+    return tweets
+
+def get_retweet_counter(tweets):
+    retweets = [tweet.retweet_count for tweet in tweets]
+    return retweets
+
+def get_time(tweets):
+    time_created = [tweet.created_at for tweet in tweets]
+
+tweets = get_tweets()
+retweets = get_retweet_counter(tweets)
+times = get_time(tweets)
+
 for tweet in tweets:
     print(tweet.text)
+for retweet in retweets:
+    print(retweet)
